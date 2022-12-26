08006dfd1ef9c643d90084ad9db9f64be97db233
ethan-stone <ethan.stone9352@gmail.com>
2022-12-26 01:10:04 -0500

    added landing page

--- web/src/pages/index.tsx
+++ web/src/pages/index.tsx
@@ -1,4 +1,5 @@
 import { type NextPage } from "next";
+import { signIn } from "next-auth/react";
 import Head from "next/head";
 import AuthButton from "../components/auth-button";
 
@@ -13,14 +14,67 @@ const Home: NextPage = () => {
         />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-neutral-900 to-neutral-700">
-        <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16 ">
-          <h1 className="text-5xl font-extrabold tracking-tight text-white sm:text-[5rem]">
-            inquire
-          </h1>
+      <main className="flex min-h-screen flex-grow flex-col bg-gradient-to-tr from-neutral-900 to-neutral-700">
+        <div className="flex flex-grow bg-[url('/background_pattern.svg')]">
+          <div className="px-40">
+            <div className="flex items-center justify-between py-6 md:justify-start md:space-x-10">
+              <div className="flex justify-start text-xl text-white lg:w-0 lg:flex-1">
+                inquire
+                {/* <Image
+              className="rounded-lg"
+              height={75}
+              width={75}
+              src="/inquire_logo.png"
+              alt="Inquire Logo"
+              TODO: replace with logo
+            /> */}
+              </div>
+              <div className="hidden items-center justify-end md:flex md:flex-1 lg:w-0">
+                <AuthButton />
+              </div>
+            </div>
 
-          <div className="flex flex-col items-center gap-2">
-            <AuthButton />
+            <div className="flex flex-grow flex-row items-center gap-12 pt-12 text-white">
+              <div className="flex w-3/5 flex-col gap-12">
+                <h1 className="text-5xl font-extrabold tracking-tight text-white sm:text-[5rem]">
+                  inquire
+                </h1>
+                <p className="text-2xl text-white/50">
+                  Interact with a variety of ai personas to solve any problem
+                  you have. Inquire makes sure you get the most out of your ai
+                  assistant.
+                </p>
+                <div>
+                  <button
+                    className="rounded-xl bg-white/10 px-10 py-3 font-semibold text-white no-underline transition hover:bg-white/20"
+                    onClick={() =>
+                      signIn(undefined, { callbackUrl: "/account" })
+                    }
+                  >
+                    Sign Up
+                  </button>
+                </div>
+              </div>
+              <div className="flex w-2/5">
+                <div className="flex flex-col gap-12 rounded-xl bg-black/75 p-12 font-mono text-2xl text-white/75">
+                  <div>
+                    <p className="py-2">/trainer</p>
+                    <p>How can I build muscle?</p>
+                  </div>
+                  <div>
+                    <p className="py-2">/programmer</p>
+                    <p>
+                      What is the cause of this error? &apos;x&apos; is possibly
+                      &apos;undefined&apos; .ts(18048)
+                    </p>
+                  </div>
+                  <div>
+                    <p className="py-2">/writer</p>
+                    <p>What are the core elements of a good horror story?</p>
+                  </div>
+                </div>
+              </div>
+            </div>
           </div>
         </div>
       </main>
