adc09f0758b1b32ddeed11f3d58897479a54038e
ethan-stone <ethan.stone9352@gmail.com>
2022-12-26 01:10:04 -0500

    added landing page

--- web/src/components/auth-button.tsx
+++ web/src/components/auth-button.tsx
@@ -6,7 +6,7 @@ const AuthButton: React.FC = () => {
   return (
     <div className="flex flex-col items-center justify-center gap-4">
       <button
-        className="rounded-full bg-white/10 px-10 py-3 font-semibold text-white no-underline transition hover:bg-white/20"
+        className="rounded-xl bg-white/10 px-10 py-3 font-semibold text-white no-underline transition hover:bg-white/20"
         onClick={
           sessionData
             ? () => signOut()
