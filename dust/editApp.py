047a36d2c51a5d8221b8b8d4d88e8f72a2c98a47
Lucas-Kohorst <kohorstlucas@gmail.com>
2022-12-30 19:59:45 +0000

    updated db

--- dust/editApp.py
+++ dust/editApp.py
@@ -8,7 +8,7 @@ load_dotenv()
 
 def main():
     with sync_playwright() as p:
-        browser = p.chromium.launch(headless=False)
+        browser = p.chromium.launch(headless=True)
         page = browser.new_page()
         page.goto('https://dust.tt')
 
