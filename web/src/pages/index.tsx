3df09e4119436d69e29997261fd23d799a84bb57
ethan-stone <ethan.stone9352@gmail.com>
2023-01-15 19:30:12 -0500

    added responsiveness to grids in personas search

--- web/src/pages/index.tsx
+++ web/src/pages/index.tsx
@@ -57,7 +57,7 @@ const PersonaHits: React.FC = () => {
   const { hits } = useHits<AlgoliaPersona>();
 
   return (
-    <div className="grid grid-cols-4 gap-2 py-4">
+    <div className="grid gap-2 py-4 lg:grid-cols-3 xl:grid-cols-4">
       {hits.map((h, idx) => {
         return <PersonaHit key={idx} personaHit={h} />;
       })}
