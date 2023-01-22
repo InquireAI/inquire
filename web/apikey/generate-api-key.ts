8d7033013e7f7036a4baaf0dde5471385bdd62ca
ethan-stone <ethan.stone9352@gmail.com>
2023-01-21 19:43:52 -0500

    added helper to quickly generate an api key

---
+++ web/apikey/generate-api-key.ts
@@ -0,0 +1,21 @@
+import { randomBytes, createHash } from "crypto";
+
+function generateKey() {
+  const buffer = randomBytes(32);
+  return buffer.toString("hex");
+}
+
+function hash(s: string) {
+  return createHash("sha256").update(s).digest("hex");
+}
+
+function main() {
+  const apikey = generateKey();
+  const hashedApiKey = hash(apikey);
+  console.log({
+    apikey,
+    hashedApiKey,
+  });
+}
+
+main();
