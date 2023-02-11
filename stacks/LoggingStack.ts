b14e35ed0906ed4d8971e5f785e7d19d0458674c
ethan-stone <ethan.stone9352@gmail.com>
2023-02-10 22:06:08 -0500

    refactor: use ecr repo instead of binary

--- stacks/LoggingStack.ts
+++ stacks/LoggingStack.ts
@@ -1,9 +1,10 @@
-import { Function, StackContext } from "sst/constructs";
+import { StackContext } from "sst/constructs";
 import * as destinations from "aws-cdk-lib/aws-logs-destinations";
 import * as lambda from "aws-cdk-lib/aws-lambda";
+import * as ecr from "aws-cdk-lib/aws-ecr";
 import { z } from "zod";
-import path from "path";
 import { Duration } from "aws-cdk-lib";
+import { ServicePrincipal } from "aws-cdk-lib/aws-iam";
 
 const EnvSchema = z.object({
   GRAFANA_API_KEY: z.string(),
@@ -14,14 +15,14 @@ const EnvSchema = z.object({
 export function LoggingStack({ stack }: StackContext) {
   const env = EnvSchema.parse(process.env);
 
+  const grafanaLambdaPromtailRepo = ecr.Repository.fromRepositoryName(
+    stack,
+    "GrafanaLambdaPromtailRepo",
+    "grafana/lambda-promtail"
+  );
   const ingesterFunction = new lambda.Function(stack, "LogIngester", {
     runtime: lambda.Runtime.GO_1_X,
-    code: lambda.Code.fromAsset(
-      path.join(
-        path.resolve(),
-        "./services/functions/grafana-promtail/main.zip"
-      )
-    ),
+    code: lambda.Code.fromEcrImage(grafanaLambdaPromtailRepo),
     handler: "main",
     memorySize: 512,
     timeout: Duration.minutes(1),
@@ -32,6 +33,12 @@ export function LoggingStack({ stack }: StackContext) {
       USERNAME: env.GRAFANA_USERNAME,
       WRITE_ADDRESS: env.GRAFANA_WRITE_ADDRESS,
     },
+  });
+
+  ingesterFunction.addPermission("CloudwatchLogGroupInvoke", {
+    principal: new ServicePrincipal("logs.amazonaws.com"),
+    action: "lambda:InvokeFunction",
+    sourceArn: `arn:aws:logs:us-east-1:${stack.account}:log-group:*:*`,
   });
 
   const lambdaDestination = new destinations.LambdaDestination(
