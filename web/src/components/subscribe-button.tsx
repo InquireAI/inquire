c88e8f114441aa9dcd77550f6c28140be06bcaee
ethan-stone <ethan.stone9352@gmail.com>
2023-02-19 22:12:40 -0500

    fix relative urls

--- web/src/components/subscribe-button.tsx
+++ web/src/components/subscribe-button.tsx
@@ -15,8 +15,8 @@ const SubscribeButton: React.FC = () => {
         className="flex w-full items-center justify-center rounded-md border border-neutral-200 bg-neutral-100 px-10 py-3 font-semibold text-neutral-700 no-underline transition hover:cursor-pointer hover:border-transparent hover:bg-gradient-to-br hover:from-neutral-900 hover:to-neutral-700 hover:text-white"
         onClick={() =>
           createCheckoutSession({
-            successUrl: `billing/account`,
-            cancelUrl: `billing/account`,
+            successUrl: `/billing/account`,
+            cancelUrl: `/billing/account`,
           })
         }
       >
